6e50e9a94c491c451bbab20a471dab75ed02881b
Johan Lindefelt <johan.lindefelt@garrison.se>
2022-12-05 14:31:13 +0100

    add parsing of the stacks

--- day5/index.js
+++ day5/index.js
@@ -1,41 +1,49 @@
 import { readFileSync } from 'fs';
-const alpha = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-let data = readFileSync('day5/input.txt', 'utf8').split('\r\n')
+
+const input = readFileSync('day5/input.txt', 'utf8').split('\r\n')
 
 const instructionPattern = /move (\d*) from (\d*) to (\d*)/
 
-let res1 = solve1(data)
+const res1 = solve1(input)
 console.log(res1)
 
-let res2 = solve2(data)
+const res2 = solve2(input)
 console.log(res2)
 
+
+function parseStacks(input){
+    let stacks = Array(9).fill().map(()=>Array())
+    input.map((row,index) => {
+        for (let i = 1; i < row.length; i+=4) {
+            const element = row[i];
+            stacks[index].push(element)
+        }
+    })
+    //Transpose the matrix
+    stacks = stacks[0].map((col,i)=>stacks.map(row => row [i]))
+    //Reverse the stack order and remove empty/undefined cells
+    return stacks.map(stack=>stack.reverse().filter(e => e!==' '&&e!==undefined))
+}
+
 function parseInstruction(instruction){
-    let match = instruction.match(instructionPattern)
+    const match = instruction.match(instructionPattern)
     return {count: match[1],
             from: match[2]-1,
             to: match[3]-1}
 }
-function solve1(data){
 
-    let stacks = Array(9).fill().map(()=>Array())
-    stacks[0].push(...'DLJRVGF'.split(''))
-    stacks[1].push(...'TPMBVHJS'.split(''))
-    stacks[2].push(...'VHMFDGPC'.split(''))
-    stacks[3].push(...'MDPNGQ'.split(''))
-    stacks[4].push(...'JLHNF'.split(''))
-    stacks[5].push(...'NFVQDGTZ'.split(''))
-    stacks[6].push(...'FDBL'.split(''))
-    stacks[7].push(...'MJBSVDN'.split(''))
-    stacks[8].push(...'GLD'.split(''))
+function solve1(input){
+    const inputStacks = input.slice(0,8)
+    const inputMoves = input.slice(10)
+    let stacks = parseStacks(inputStacks)
 
-    data.map(row => {
+
+    inputMoves.map(row => {
         let inst = parseInstruction(row)
 
         for (let index = 0; index <inst.count; index++) {
             let v = stacks[inst.from].pop()
             stacks[inst.to].push(v)
-            
         }
         
     })
@@ -43,20 +51,12 @@ function solve1(data){
     return stacks.map(e => e[e.length-1]).join('')
    
 }
-function solve2(data){
-    
-    let stacks = Array(9).fill().map(()=>Array())
-    stacks[0].push(...'DLJRVGF'.split(''))
-    stacks[1].push(...'TPMBVHJS'.split(''))
-    stacks[2].push(...'VHMFDGPC'.split(''))
-    stacks[3].push(...'MDPNGQ'.split(''))
-    stacks[4].push(...'JLHNF'.split(''))
-    stacks[5].push(...'NFVQDGTZ'.split(''))
-    stacks[6].push(...'FDBL'.split(''))
-    stacks[7].push(...'MJBSVDN'.split(''))
-    stacks[8].push(...'GLD'.split(''))
+function solve2(input){
+    const inputStacks = input.slice(0,8)
+    const inputMoves = input.slice(10)
+    let stacks = parseStacks(inputStacks)
 
-    data.map(row => {
+    inputMoves.map(row => {
         let inst = parseInstruction(row)
 
             let stackFrom = stacks[inst.from]
