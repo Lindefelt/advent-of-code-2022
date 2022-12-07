8c3251cd057268bdae88ec031534028254d49ea2
Johan Lindefelt <johan.lindefelt@garrison.se>
2022-12-07 12:39:54 +0100

    cleanup day7

--- day07/index.js
+++ day07/index.js
@@ -27,80 +27,38 @@ function solve1(dirs){
 
 function calculateDirSizes(input){
     let pwd = []
-    let row = ''
     let files = new Map()
     let dirNames = new Set('/')
-    while (row = input.shift()) {
-    
+
+    input.forEach(row => {
         if(row.startsWith('$ cd')){
-           let dir  =  row.slice(4).trim()
-           if(dir === '..'){
-            pwd.pop()
-           }else if(dir === '/'){
-            pwd = [dir]
-           }
-           else{
-            pwd.push(dir+'/')
-           }
-        }
-        else if(row.startsWith('$ ls')){
+            let dir  =  row.slice(4).trim()
+            if(dir === '..'){
+             pwd.pop()
+            }else if(dir === '/'){
+             pwd = [dir]
+            }
+            else{
+             pwd.push(dir+'/')
+            }
+         }else if (row.startsWith('$ ls')){
             
          }
-        else if(row.startsWith('dir')) {
-            dirNames.add(pwd.join('')+row.slice(4))
-        }
-        else{
-            let parts = row.split(' ')
-            files.set(pwd.join('')+parts[1],Number.parseInt(parts[0]))
-        }
-    }
+         else if(row.startsWith('dir')) {
+             dirNames.add(pwd.join('')+row.slice(4))
+         }
+         else{
+             let parts = row.split(' ')
+             files.set(pwd.join('')+parts[1],Number.parseInt(parts[0]))
+         }
+        })
 
-    let dirs = Array.from(dirNames).map(dir => { 
-        let size = Array.from(files.keys())
-        .filter(file => file.startsWith(dir))
-        .reduce((acc,curr) => acc+files.get(curr),0)
-        return {dir,size}
-    })
+     let dirs = Array.from(dirNames).map(dir => { 
+         let size = Array.from(files.keys())
+         .filter(file => file.startsWith(dir))
+         .reduce((acc,curr) => acc+files.get(curr),0)
+         return {dir,size}
+        })
 
     return dirs
 }
-
-
-
-
-// function solve1(input){
-//     let pwd = []
-//     let tree = {value:'', dir: true, size: 0 ,nodes : []}
-//     let currNode = tree
-//     let i = 0
-//     let row = ''
-//     while (row = input.shift()) {
-    
-//         if(row.startsWith('$ cd')){
-//            let dir  =  row.slice(4).trim()
-//            if(dir === '..'){
-//             pwd.pop()
-//            }else if(dir === '/'){
-//             pwd = [dir]
-//             currNode.value = dir
-//            }
-//            else{
-//             pwd.push(dir)
-//             currNode = currNode.nodes.filter(e => e.value===dir)[0]
-//            }
-           
-//         }
-//         else if(row.startsWith('$ ls')){
-            
-//          }
-//         else if(row.startsWith('dir')) {
-//             currNode.nodes.push({value: row.slice(4),size: 0, nodes: [] })
-//         }
-//         else{
-//             let parts = row.split(' ')
-//             currNode.nodes.push({value: parts[1],size: parts[0]})
-//         }
-//     }
-
-// }
-
