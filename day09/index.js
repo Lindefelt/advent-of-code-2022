84f6f576719eb19948a233e2ec4aae4dd833ade2
Johan Lindefelt <johan.lindefelt@garrison.se>
2022-12-10 20:40:41 +0100

    day 9 part 2

--- day09/index.js
+++ day09/index.js
@@ -1,9 +1,11 @@
 import { readFileSync } from 'fs';
 
 const input = readFileSync('day09/input.txt', 'utf8').split('\r\n')
+const test1 = readFileSync('day09/test.txt', 'utf8').split('\r\n')
+const test2 = readFileSync('day09/test2.txt', 'utf8').split('\r\n')
 
 console.log(solve1(input))
-// console.log(solve2(matrix))
+console.log(solve2(input))
 
 function solve1(input) {
     let head = { x: 0, y: 0 }
@@ -16,7 +18,7 @@ function solve1(input) {
             switch (move.dir) {
                 case 'R':
                     head.x++
-                    
+
                     break;
 
                 case 'L':
@@ -34,53 +36,99 @@ function solve1(input) {
                     break;
 
             }
-            moveRelativeTo(head,tail)
-            visited.add(tail.x+','+tail.y)
+            moveRelativeTo(head, tail)
+            visited.add(tail.x + ',' + tail.y)
 
         }
 
     });
-    
+
     let res = visited.size
     return res
 }
 
-function moveRelativeTo(h,t){
-    if(h.x - t.x > 1){
-       t.x++
-       if(h.y > t.y||h.y < t.y){
-        t.y=h.y
-       }
+function solve2(input) {
+    let head = { x: 0, y: 0 }
+    let knots = Array(9).fill(null).map(e => ({ x: 0, y: 0 }))
+
+    let visited = new Set()
+    input.forEach(element => {
+        let move = parseMove(element)
+        for (let i = 0; i < move.steps; i++) {
+            switch (move.dir) {
+                case 'R':
+                    head.x++
+
+                    break;
+
+                case 'L':
+                    head.x--
+
+                    break;
+
+                case 'U':
+                    head.y++
+
+                    break;
+                case 'D':
+                    head.y--
+
+                    break;
+
+            }
+            knots.reduce(moveRelativeTo, head)
+            visited.add(knots[8].x + ',' + knots[8].y)
+
+        }
+
+    });
+
+    let res = visited.size
+    return res
+}
+
+function moveRelativeTo(h, t) {
+    if (h.x - t.x > 1) {
+        t.x++
+        if (h.y > t.y) {
+            t.y++
+        }
+        if (h.y < t.y) {
+            t.y--
+        }
 
     }
-    if(t.x - h.x > 1){
+    if (t.x - h.x > 1) {
         t.x--
-        if(h.y > t.y||h.y < t.y){
-         t.y=h.y
+        if (h.y > t.y) {
+            t.y++
+        }
+        if (h.y < t.y) {
+            t.y--
         }
     }
-    if(h.y - t.y > 1){
+    if (h.y - t.y > 1) {
         t.y++
-        if(h.x > t.x||h.x < t.x){
-         t.x=h.x
+        if (h.x > t.x) {
+            t.x++
         }
- 
-     }
-     if(t.y - h.y > 1){
-         t.y--
-         if(h.x > t.x||h.x < t.x){
-          t.x=h.x
-         }
-     }
+        if (h.x < t.x) {
+            t.x--
+        }
 
+    }
+    if (t.y - h.y > 1) {
+        t.y--
+        if (h.x > t.x) {
+            t.x++
+        }
+        if (h.x < t.x) {
+            t.x--
+        }
+    }
+    return t
 }
 function parseMove(input) {
     let move = input.split(' ')
     return ({ dir: move[0], steps: Number.parseInt(move[1]) })
 }
-
-// function solve2() {
-//     let res = 0
-//     return res
-// }
-
